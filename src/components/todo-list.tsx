877b080ee7ccd7647a3a02e66494ea85e207509d
Ali Askari <askarialidev@gmail.com>
2024-11-20 17:23:11 +0330

    add: todo list

--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -1,13 +1,25 @@
 "use client";
 
 import { useTodoStore } from "@/stores/useTodoStore";
-import { Paper } from "@mui/material";
+import { Paper, Stack, Typography } from "@mui/material";
+import TodoItem from "./todo-item";
+import React from "react";
 
 export default function TodoList() {
-  const todoList = useTodoStore(state => state.todos)
+  const todoList = useTodoStore((state) => state.todos);
   return (
     <Paper className="p-4">
-      <div>{JSON.stringify(todoList)}</div>
+      {todoList.length ? (
+        <Stack>
+          {todoList.map((todo) => (
+            <TodoItem key={todo.id} data={todo} />
+          ))}
+        </Stack>
+      ) : (
+        <Typography align="center" variant="body2" color="textDisabled">
+          No To-Do yet
+        </Typography>
+      )}
     </Paper>
   );
 }
