21eeff8acfc80eb9e38302ac592d10ccf42f58a7
Ali Askari <askarialidev@gmail.com>
2024-11-21 20:25:05 +0330

    test: check marking todo as done

--- __tests__/todo-item.test.tsx
+++ __tests__/todo-item.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import TodoItem from "@/components/todo-item";
+import UserEvent from "@testing-library/user-event";
 
 describe("TodoItem Component", () => {
   const mockTodo = {
@@ -9,8 +10,13 @@ describe("TodoItem Component", () => {
     createdAt: "2024-11-21T10:00:00.000Z",
   };
 
+  const onDelete = vi.fn();
+  const onToggle = vi.fn();
+
   const setup = () => {
-    render(<TodoItem data={mockTodo} />);
+    render(
+      <TodoItem data={mockTodo} onDelete={onDelete} onToggle={onToggle} />
+    );
   };
 
   it("should display the todo title", () => {
@@ -18,9 +24,12 @@ describe("TodoItem Component", () => {
     expect(screen.getByText(mockTodo.title)).toBeInTheDocument();
   });
 
-  it("should mark the todo as done when the checkbox is clicked", () => {
+  it("should mark the todo as done when the checkbox is clicked", async () => {
     setup();
-    throw new Error("Not implemented yet");
+    const checkbox = screen.getByRole("checkbox");
+    const user = UserEvent.setup();
+    await user.click(checkbox);
+    expect(onToggle).toHaveBeenCalled();
   });
 
   it("should delete the todo when delete button is clicked", () => {
